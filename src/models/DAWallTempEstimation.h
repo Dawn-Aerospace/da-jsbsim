7a68601940dfd71b71b24109c5d1c3d35740025f
Caleb <caleb@dawnaerospace.com>
2023-02-02 15:42:57 +1300

    add testing for wall temp

--- src/models/DAWallTempEstimation.h
+++ src/models/DAWallTempEstimation.h
@@ -14,40 +14,46 @@ namespace JSBSim {
 
 class DAWallTempEstimation {
 public:
-    explicit DAWallTempEstimation(JSBSim::FGFDMExec *_fdmex);
-    double GetWallTempEstimateCelsius(double chord);
+    DAWallTempEstimation() {};
+    DAWallTempEstimation(JSBSim::FGFDMExec *_fdmex);
+    double GetWallTempEstimateCelsius(double chord, double _machSpeed);
 
-private:
-    FGFDMExec* fdmex_;
+protected:
     std::shared_ptr<FGAtmosphere> atmosphere_;
-
-    double HeatBalance(double estimate) const;
-    std::tuple<double, double> static CalculateCpAndGamma(double estimate);
-    double NewtonRaphson();
-
-    double machSpeed;
     double chord_m;
+    double machSpeed;
     double airTemp_K;
     double airPressure_Pa;
     double kinematicViscosity;
     double absoluteViscosity;
     double soundSpeed_ms;
+    double maxIterations=30;
+    int flowType_ = 1; //flow type: 0 = laminar, 1 = turbulent
+    virtual void SetInputs();
+
+    double HeatBalance(double estimate) const;
+    std::tuple<double, double> static CalculateCpAndGamma(double estimate);
+    double NewtonRaphson();
+
+private:
+    FGFDMExec* fdmex_;
     double velocity_ms;
     double reynoldsNumber;
     double cfi, NN, C, N, a, b;
 
     double startingGuess=500;
-    double tol=1E-2;
+    double tol_fx=1E-2;
+    double tol_df=1E-16;
     double dx=1E-1;
-    double maxIterations=30;
+    int xMin = 10;
+    int xMax = 10000;
     double w = 0.76;
     double sg = 5.67E-8; //stefan-boltzmann constant
     double emissivity_ = 0.9; //emissivity anodised aluminum or white paint or black paint
-    int flowType_ = 1; //flow type: 0 = laminar, 1 = turbulent
 
     double KELVIN_TO_CELSIUS_ = -273.15;
     double FEET_TO_METER_ = 0.3048;
-    double RANKINE_TO_KELVIN_ = 1/1.8;
+    double RANKINE_TO_KELVIN_ = 1.0/1.8;
     double PSF_TO_PASCAL_ = 47.880208;
 };
 }
