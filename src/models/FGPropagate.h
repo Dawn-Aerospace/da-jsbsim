2f71d99135b669005d5a1a824cc9ebc47be36886
Andrew Liang <a.liang@dawnaerospace.com>
2023-04-13 15:37:42 +1200

    expose quaternion elements

--- src/models/FGPropagate.h
+++ src/models/FGPropagate.h
@@ -410,6 +410,16 @@ public:
   */
   double GetSinEuler(int idx) const { return VState.qAttitudeLocal.GetSinEuler(idx); }
 
+  /** Retrieves an element of the vehicle Quaternion vector.
+      Retrieves an element of the quaternion that stores the vehicle orientation 
+      relative to the Local frame. The relevant arguments for the elements returned 
+      by this call are the elements of the vector starting at index 1 
+      (e.g. q0=1, q1=2, q2=3, q2=4).
+      units radians
+      @return An Quaternion element.
+  */
+  double GetQuaternion(int idx) const { return VState.qAttitudeLocal.Entry(idx); }
+
   /** Returns the current altitude rate.
       Returns the current altitude rate (rate of climb).
       units ft/sec
